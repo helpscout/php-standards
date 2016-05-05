ab0642c93c52275d6732c56c13842e276618e55f
Craig Davis <craig@helpscout.com>
2016-05-05 15:50:27 -0600

    Add Help Scout PHP Code Standards

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# php-standards
-Help Scout PHP Code Standards
+# Help Scout PHP Code Standards
+================================================================================
+>
+
+This repository includes the [Help Scout][dev] coding standard definition for [PHP Codesniffer][phpcs] and resources. The PHP_CodeSniffer standard will never be 100% accurate, but should be viewed as a strong set of guidelines while writing software at Help Scout.
+
+These are most commonly used with the [Pre-Commit][pre-commit] commit hooks for linting source code with the [php-cs hook][php-cs].
+
+[dev]: http://developer.helpscout.net/
+[phpcs]: http://pear.php.net/PHP_CodeSniffer
+[pre-commit]: http://pre-commit.com/
+[php-cs]: https://github.com/hootsuite/pre-commit-php#php-cs
---
+++ Sniffs/Functions/DisallowXDebugSniff.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This sniff prohibits the use of xdebug functions
+ *
+ * @author     Platform Team <developer@helpscout.net>
+ * @copyright  2015 Help Scout
+ */
+
+/**
+ * This sniff prohibits the use of xdebug functions
+ */
+class HelpScout_Sniffs_Functions_DisallowXDebugSniff extends Generic_Sniffs_PHP_ForbiddenFunctionsSniff
+{
+    public $forbiddenFunctions = [
+        'var_dump'                      => null,
+        'xdebug_break'                  => null,
+        'xdebug_call_class'             => null,
+        'xdebug_call_file'              => null,
+        'xdebug_call_function'          => null,
+        'xdebug_call_line'              => null,
+        'xdebug_disable'                => null,
+        'xdebug_enable'                 => null,
+        'xdebug_get_collected_errors'   => null,
+        'xdebug_get_headers'            => null,
+        'xdebug_is_enabled'             => null,
+        'xdebug_memory_usage'           => null,
+        'xdebug_peak_memory_usage'      => null,
+        'xdebug_start_error_collection' => null,
+        'xdebug_stop_error_collection'  => null,
+        'xdebug_time_index'             => null
+    ];
+}
+
+/* End of file DisallowXDebugSniff.php */
